80291ae24efa81d42e2d0e8dbb1bf02ee119b949
Itay Issashar <itayishr@gmail.com>
2023-08-02 17:44:56 +0300

    Added response model for stats and fixed method comment

--- routers/comparator_routes.py
+++ routers/comparator_routes.py
@@ -10,14 +10,15 @@ FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix='/api/v1', tags=['Word Comparator'])
+router = APIRouter(prefix="/api/v1", tags=["Word Comparator"])
 
 
 class ResponseModel(BaseModel):
     totalWords: int
     totalRequests: int
     avgProcessingTimeNs: int
- 
+
+
 @router.get("/similar", status_code=200)
 async def similar(word_to_compare: str) -> JSONResponse:
     """
@@ -27,10 +28,12 @@ async def similar(word_to_compare: str) -> JSONResponse:
     return JSONResponse({SIMILAR: similar_words})
 
 
-@router.get("/stats", status_code=200,response_model=ResponseModel)
+@router.get("/stats", status_code=200, response_model=ResponseModel)
 async def stats() -> JSONResponse:
     """
-    Handle request to search for words similar to passed word_to_compare.
+    Return application statistics. 
     """
-    similar_words = []
-    return JSONResponse({SIMILAR: similar_words})
+    # TODO add stats logic.
+    response_json = {"totalWords": 1, "totalRequests": 1, "avgProcessingTimeNs": 1}
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=response_json)
