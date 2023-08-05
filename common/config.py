7e41af1827fc1b5753edef53170125d09bc8575b
itayishr <37871040+itayishr@users.noreply.github.com>
2023-08-05 20:35:25 +0300

    Update config.py

--- common/config.py
+++ common/config.py
@@ -1,7 +1,6 @@
 import os
 
 # Environment variables
-ELASTIC_HOST: str = os.environ.get("ELASTICSEARCH_HOSTS", "http://localhost:9200")
 REDIS_HOST: str = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT: int = os.environ.get("REDIS_PORT", "6780")
 CELERY_BROKER_URL = os.environ.get(
