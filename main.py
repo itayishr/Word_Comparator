06019109eac4b78847c8378f04f3eb749f9cb2f2
Itay Issashar <itayishr@gmail.com>
2023-08-08 11:27:43 +0300

    Added load balancer and modified yml so that internal port will not be exposed (requests should go only through nginx).

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
-from routers import comparator_routes
 import uvicorn
+from fastapi import FastAPI
+
+from routers import comparator_routes
 
 app = FastAPI(
     title="Word Comparator Rest API",
     description="Word Comparator service implemented with FastAPI, to supply the Word Comparator with new requests.",
     version="1.0.0",
+    root_dir="/"
 )
 app.include_router(comparator_routes.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
